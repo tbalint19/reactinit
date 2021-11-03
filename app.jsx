6e1ab43c13c67332a073cb99d52d2fa2cfd61055
tbalint19 <tothbalint719@gmail.com>
2021-11-03 13:12:03 +0100

    initial

---
+++ app.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+
+    return (
+        <div id="app-container">
+            <nav>This is a nav</nav>
+            <article>This is an article</article>
+            <footer>This is a footer</footer>
+        </div>
+    )
+}
