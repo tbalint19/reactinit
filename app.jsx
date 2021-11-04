af8045de3bfdc8c88ab230587b9b8eee4190890f
tbalint19 <tothbalint719@gmail.com>
2021-11-04 14:10:34 +0100

    map

--- app.jsx
+++ app.jsx
@@ -1,8 +1,19 @@
 const App = () => {
 
+    const title = "This is a nav"
+
+    const people = [
+        { name: "bela", age: 30 },
+        { name: "lajos", age: 40 },
+        { name: "kazmer", age: 50 }
+    ]
+
     return (
         <div id="app-container">
-            <nav>This is a nav</nav>
+            <nav>{ title }</nav>
+            {
+                people.map(person => <p>{ person.name } - { person.age }</p>)
+            }
             <article>This is an article</article>
             <footer>This is a footer</footer>
         </div>
